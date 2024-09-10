28a98a8c992ca4310e1c08f1bc14887470929280
Wilson-Kan <Wilson-Kan>
2024-09-10 20:33:26 +0000

    PSI data gathered, the model scoring still seem broken

--- Modeling/ApplicationV2/8. Monitoring Data.py
+++ Modeling/ApplicationV2/8. Monitoring Data.py
@@ -4,6 +4,18 @@
 
 # COMMAND ----------
 
+import datetime
+import dateutil.relativedelta
+
+t_date = datetime.datetime.today()
+st_date_1 = t_date - dateutil.relativedelta.relativedelta(years=1, months=3)
+st_date_2 = t_date - dateutil.relativedelta.relativedelta(years=1)
+st_date_1 = st_date_1.replace(day=1)
+st_date_2 = st_date_2.replace(day=1)
+t_date = t_date.replace(day=1)
+
+# COMMAND ----------
+
 from pyspark.sql import SparkSession
 from xgboost import XGBClassifier
 import math
@@ -24,9 +36,13 @@ model_list = [
     "tims_thick_no_sp.json",
     "tims_thin_no_sp.json",
 ]
-
-checkyear = 2023
-checkmonth = 8
+model_id_list = [
+    "neo_thick",
+    "neo_thin",
+    "neo_subprime",
+    "tims_thick",
+    "tims_thin",
+]
 
 # COMMAND ----------
 
@@ -35,6 +51,7 @@ raw_data = spark.sql(
   f"""
   select
   ea._id,
+  ms.applicationId,
   ea.userId,
   ea.userReportsMetadataSnapshotId,
   ea.brand,
@@ -125,132 +142,280 @@ from
   inner join neo_raw_production.user_service_users as usu on ea.userId = usu._id
 where
   decision = "APPROVED"
-  and year(ea.createdAt) = {checkyear}
-  and month(ea.createdAt) = {checkmonth}
+  and ea.createdAt between '{st_date_1}' and '{st_date_2}'
   """
 )
+
+default_list = spark.sql(
+  f"""
+    select distinct applicationId, 1 as def
+      FROM
+        neo_trusted_analytics.earl_account
+      where
+      (
+        daysPastDue >= 90
+        or chargedOffReason not in ("N/A")
+      )
+      and referenceDate between '{st_date_1}' and '{t_date}' 
+  """
+)
+
+score_me = raw_data.alias("a").join(default_list.alias("b"), raw_data.applicationId == default_list.applicationId, how="left").select("a.*", "b.def")
+score_me = score_me.na.fill(value=0,subset=["def"])
+score_me = score_me.toPandas()
+
+# COMMAND ----------
+
+# Export/Import pickle file
+
+import pickle
+
+with open(
+    "/Workspace/Users/wilson.kan@neofinancial.com/ApplicationV2/pkls/appl_v2_monitoring_temp.pkl",
+    "wb",
+) as f:  # open a text file
+    pickle.dump(score_me, f)
+
+# with open(
+#     "/Workspace/Users/wilson.kan@neofinancial.com/ApplicationV2/pkls/appl_v2_monitoring_temp.pkl",
+#     "rb",
+# ) as f:  # Correctly opening the file in binary read mode
+#     score_me = pickle.load(f)
+
+# COMMAND ----------
+
+score_me["Subprime"] = (score_me["creditScore"] < 640) * 1
+score_me["Thin"] = ((score_me["GO14"] <= 24) | (score_me["AT01"] <= 0)) * 1
+
+def model_id(row):
+    if row["brand"] == "NEO":
+        if row["Subprime"] == 1:
+            return "neo_subprime"
+        elif row["Thin"] == 1:
+            return "neo_thin"
+        else:
+            return "neo_thick"
+    elif row["brand"] == "SIENNA":
+        if row["Thin"] == 1:
+            return "tims_thin"
+        else:
+            return "tims_thick"
+    return "other"
+
+
+score_me["model_id"] = score_me.apply(model_id, axis=1)
+
+# COMMAND ----------
+
+from sklearn.metrics import roc_auc_score
+
+res = {'model_id': [], 'date':[], 'count': [], 'def_count': [], 'auc': []}
+
+for i in range(len(model_list)):
+  bst.load_model(model_save_path + model_list[i])
+  test_dat = score_me[score_me['model_id'] == model_id_list[i]]
+  test_dat.loc[:,'raw_pred'] = - 1 * bst.predict_proba(test_dat.loc[:,bst.feature_names_in_])[:,1]
+  auc = 1
+  def_c = test_dat['def'].sum()
+  if def_c > 0:
+    auc = 2 * roc_auc_score(test_dat.loc[:,'def'], test_dat.loc[:,'raw_pred']) - 1
+  res['model_id'].append(model_id_list[i])
+  res['date'].append(f"{st_date_1.date()} - {st_date_2.date()}")
+  res['auc'].append(auc)
+  res['def_count'].append(def_c)
+  res['count'].append(test_dat.shape[0])
+
+# COMMAND ----------
+
+pd.DataFrame(res)
 
 # COMMAND ----------
 
 #prepare initial data for psi/csi
 #save to wk_appl_v2_for_stability
+import numpy as np
+import random
+
+from sklearn.model_selection import train_test_split
+
+psi_save = {}
+
+with open(
+    "/Workspace/Users/wilson.kan@neofinancial.com/ApplicationV2/pkls/appl_neo_v2_modeling_ready.pkl",
+    "rb",
+) as f:  # Correctly opening the file in binary read mode
+    modeling_dummy_df = pickle.load(f)
+
+modeling_intime = modeling_dummy_df[
+    modeling_dummy_df["month_end"] < np.datetime64("2023-06-30")
+]
+modeling_intime = modeling_dummy_df[
+    modeling_dummy_df["month_end"] > np.datetime64("2022-12-31")
+]
+modeling_intime = modeling_intime[
+    modeling_intime["thin"] + modeling_intime["subprime"] == 0]
+
+# dropping duplicated features
+X_train, X_test, y_train, y_test = train_test_split(
+    modeling_intime.drop(
+        ["month_end", "isdefault_1y", "originalCreditScore", "GO17"], axis=1
+    ),
+    modeling_intime["isdefault_1y"],
+    test_size=0.2,
+    random_state=213987,
+)
+
+bst.load_model(model_save_path + model_list[0])
+features = bst.feature_names_in_
+X_train = X_train[features]
+for i in features:
+  X_train['temp'], X_b = pd.cut(X_train[i], bins=10, include_lowest=True, retbins=True)
+  psi_group = X_train[['temp']].groupby('temp', observed=True).size() / X_train.shape[0]
+  psi_save[("neo_thick", i)] = (psi_group, X_b)
+
 
 # COMMAND ----------
 
-display(raw_data)
+
+# model_id_list = [
+#     "neo_thick",
+#     "neo_thin",
+#     "neo_subprime",
+#     "tims_thick",
+#     "tims_thin",
+# ]
+
+with open(
+    "/Workspace/Users/wilson.kan@neofinancial.com/ApplicationV2/pkls/appl_neo_v2_modeling_ready.pkl",
+    "rb",
+  ) as f:  # Correctly opening the file in binary read mode
+    modeling_dummy_df = pickle.load(f)
+
+modeling_intime = modeling_dummy_df[
+    modeling_dummy_df["month_end"] < np.datetime64("2023-06-30")
+]
+modeling_intime = modeling_dummy_df[
+    modeling_dummy_df["month_end"] > np.datetime64("2022-12-31")
+]
+
+modeling_intime = modeling_intime[modeling_intime["thin"] == 1]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    modeling_intime.drop(
+        ["month_end", "isdefault_1y", "originalCreditScore", "GO17"], axis=1
+    ),
+    modeling_intime["isdefault_1y"],
+    test_size=0.2,
+    random_state=459339,
+)
+
+bst.load_model(model_save_path + model_list[1])
+features = bst.feature_names_in_
+X_train = X_train[features]
+for i in features:
+  X_train['temp'], X_b = pd.cut(X_train[i], bins=10, include_lowest=True, retbins=True)
+  psi_group = X_train[['temp']].groupby('temp', observed=True).size() / X_train.shape[0]
+  psi_save[("neo_thin", i)] = (psi_group, X_b)
+
+
+# COMMAND ----------
+
+with open(
+    "/Workspace/Users/wilson.kan@neofinancial.com/ApplicationV2/pkls/appl_neo_v2_modeling_ready.pkl",
+    "rb",
+) as f:  # Correctly opening the file in binary read mode
+    modeling_dummy_df = pickle.load(f)
+
+modeling_intime = modeling_dummy_df[
+    modeling_dummy_df["month_end"] <= np.datetime64("2024-03-31")
+]
+modeling_intime = modeling_intime[modeling_intime["subprime"] == 1]
+X_train, X_test, y_train, y_test = train_test_split(
+    modeling_intime.drop(
+        ["month_end", "isdefault_1y", "originalCreditScore", "GO17"], axis=1
+    ),
+    modeling_intime["isdefault_1y"],
+    test_size=0.2,
+    random_state=651677,
+)
+bst.load_model(model_save_path + model_list[2])
+features = bst.feature_names_in_
+X_train = X_train[features]
+for i in features:
+  X_train['temp'], X_b = pd.cut(X_train[i], bins=10, include_lowest=True, retbins=True)
+  psi_group = X_train[['temp']].groupby('temp', observed=True).size() / X_train.shape[0]
+  psi_save[("neo_subprime", i)] = (psi_group, X_b)
+
+# COMMAND ----------
+
+with open(
+    "/Workspace/Users/wilson.kan@neofinancial.com/ApplicationV2/pkls/appl_tim_v2_modeling_ready_no_sp.pkl",
+    "rb",
+) as f:  # Correctly opening the file in binary read mode
+    modeling_dummy_df = pickle.load(f)
+
+modeling_intime = modeling_dummy_df[
+    modeling_dummy_df["month_end"] < np.datetime64("2023-06-30")
+]
+modeling_intime = modeling_dummy_df[
+    modeling_dummy_df["month_end"] > np.datetime64("2022-12-31")
+]    
+modeling_intime = modeling_intime[modeling_intime["thin"] == 0]
+X_train, X_test, y_train, y_test = train_test_split(
+    modeling_intime.drop(
+        ["month_end", "isdefault_1y", "originalCreditScore", "GO17"], axis=1
+    ),
+    modeling_intime["isdefault_1y"],
+    test_size=0.2,
+    random_state=213987,
+)
+
+bst.load_model(model_save_path + model_list[3])
+features = bst.feature_names_in_
+X_train = X_train[features]
+for i in features:
+  X_train['temp'], X_b = pd.cut(X_train[i], bins=10, include_lowest=True, retbins=True)
+  psi_group = X_train[['temp']].groupby('temp', observed=True).size() / X_train.shape[0]
+  psi_save[("tims_thick", i)] = (psi_group, X_b)
+
+# COMMAND ----------
+
+with open(
+        "/Workspace/Users/wilson.kan@neofinancial.com/ApplicationV2/pkls/appl_tim_v2_modeling_ready_no_sp.pkl",
+        "rb",
+    ) as f:  # Correctly opening the file in binary read mode
+        modeling_dummy_df = pickle.load(f)
+
+modeling_intime = modeling_dummy_df[
+    modeling_dummy_df["month_end"] < np.datetime64("2023-06-30")
+]
+modeling_intime = modeling_dummy_df[
+    modeling_dummy_df["month_end"] > np.datetime64("2022-12-31")
+]
+modeling_intime = modeling_intime[modeling_intime["thin"] == 1]
+X_train, X_test, y_train, y_test = train_test_split(
+    modeling_intime.drop(
+        ["month_end", "isdefault_1y", "originalCreditScore", "GO17"], axis=1
+    ),
+    modeling_intime["isdefault_1y"],
+    test_size=0.2,
+    random_state=459339,
+)
+
+bst.load_model(model_save_path + model_list[4])
+features = bst.feature_names_in_
+X_train = X_train[features]
+for i in features:
+  X_train['temp'], X_b = pd.cut(X_train[i], bins=10, include_lowest=True, retbins=True)
+  psi_group = X_train[['temp']].groupby('temp', observed=True).size() / X_train.shape[0]
+  psi_save[("tims_thin", i)] = (psi_group, X_b)
+
+# COMMAND ----------
+
+psi_save
 
 # COMMAND ----------
 
 def stability_calc(base_dat, new_dat, var):
   #sum of (init % - new % * ln ( init % / new % ) )
   pass
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-display(raw_data)
-
-# COMMAND ----------
-
-complete_feature_set = set()
-for mod_name in model_list:
-  bst.load_model(model_save_path + mod_name)
-  complete_feature_set.update(bst.feature_names_in_)
-complete_feature_set = complete_feature_set.difference(['creditScore', 'houseStat_RENT'])
-s = ""
-for i in complete_feature_set:
-  s += i + ","
-
-# COMMAND ----------
-
-score_me = spark.sql(
-    f"""
-      select
-        brand,
-        decision,
-        createdAt,
-        _id as applicationId,
-        userId,
-        creditFacility,
-        cast(
-          transunionSoftCreditCheckResult.creditScore as int
-        ) as creditScore,
-        case
-          when housingStatus = 'RENT' then 1
-          else 0
-        end as houseStat_RENT,
-        case
-          when (
-            AT01 <= 0
-            or GO14 <= 24
-          ) then 1
-          else 0
-        end as thin,
-        case
-          when (creditScore < 640) then 1
-          else 0
-        end as subprime,
-        {s[:-1]}
-      from
-        neo_views_credit_risk.wk_feature_and_target_no_hc
-      where
-        brand in ("NEO", "SIENNA")
-        and type = 'STANDARD'
-    """
-)
-score_me_df = score_me.toPandas()
-
-# COMMAND ----------
-
-def scaling_score(raw_score, lower_bound = 300, upper_bound = 850, round_down = True, inverse_scoring = True):
-  if inverse_scoring:
-    raw_score = raw_score * -1
-  min_score = min(raw_score)
-  max_score = max(raw_score)
-  scale_0_1 = (raw_score - min_score) / (max_score - min_score)
-  scale_to_tu_range = (scale_0_1 * (upper_bound - lower_bound)) + lower_bound
-  print(f"scale_0_1 = (X - {min_score} / ({max_score} - {min_score}) \n scale_to_tu_range = (scale_0_1 * (850 - 300)) + 300))")
-  if round_down:
-    return [math.floor(i) for i in scale_to_tu_range]
-  return scale_to_tu_range
-def score_upload(model, indat, upload_name):
-
-  indat.loc[:,'raw_pred'] = bst.predict_proba(indat[bst.feature_names_in_])[:,1]
-  indat.loc[:,'model_scr'] = scaling_score(indat['raw_pred'])
-
-  # indat_sp = spark.createDataFrame(indat)
-  # indat_sp.write.mode("overwrite").option("overwriteSchema", "true").saveAsTable(upload_name)
-  # return indat[['model_scr', 'decision']].groupby(['decision']).mean()
-
-# COMMAND ----------
-
-bst.load_model(model_save_path + model_list[0])
-neo_thick_df = score_me_df[(score_me_df["thin"] == 0) & (score_me_df["subprime"] == 0) & (score_me_df["brand"] == 'NEO')]
-score_upload(bst, neo_thick_df, "hive_metastore.neo_views_credit_risk.wk_neo_thick_app_v2")
-
-# COMMAND ----------
-
-bst.load_model(model_save_path + model_list[1])
-neo_thin_df = score_me_df[(score_me_df["thin"] == 1) & (score_me_df["subprime"] == 0) & (score_me_df["brand"] == 'NEO')]
-score_upload(bst, neo_thin_df, "hive_metastore.neo_views_credit_risk.wk_neo_thin_app_v2")
-
-# COMMAND ----------
-
-bst.load_model(model_save_path + model_list[2])
-neo_sp_df = score_me_df[(score_me_df["subprime"] == 1) & (score_me_df["brand"] == 'NEO')]
-score_upload(bst, neo_sp_df, "hive_metastore.neo_views_credit_risk.wk_neo_subprime_app_v2")
-
-# COMMAND ----------
-
-bst.load_model(model_save_path + model_list[3])
-tims_thick_df = score_me_df[(score_me_df["thin"] == 0) & (score_me_df["brand"] == 'SIENNA')]
-score_upload(bst, tims_thick_df, "hive_metastore.neo_views_credit_risk.wk_tims_thick_app_v2")
-
-# COMMAND ----------
-
-bst.load_model(model_save_path + model_list[4])
-tims_thin_df = score_me_df[(score_me_df["thin"] == 1) & (score_me_df["brand"] == 'SIENNA')]
-score_upload(bst, tims_thin_df, "hive_metastore.neo_views_credit_risk.wk_tims_thin_app_v2")
