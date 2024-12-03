261671d16833fdfb8dbb074f34abbc87cf6d97f8
Wilson-Kan <Wilson-Kan>
2024-12-03 19:01:06 +0000

    Moved it away from repo

--- Test/Teacher.py
+++
@@ -1,9 +0,0 @@
-from Person import Person
-
-class Teacher (Person):
-  def __init__(self, fname, lname, subject):
-    super().__init__(fname, lname)
-    self.subject = subject
-
-  def __str__(self):
-    return self.printname()
